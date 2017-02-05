b426cd23cd26b5b53f5bdce51d763b52584d4df1
Karol-Regula <kregula@stuy.edu>
2017-02-04 20:31:12 -0500

    Iterated through csv files and matched students to courses by id.

--- db_builder.py
+++ db_builder.py
@@ -5,4 +5,18 @@ server = MongoClient('159.89.150.100')
 db = server.dbName
 c = db.students
 
-d1 = csv.dictReader
+d1 = csv.DictReader(open("peeps.csv"))
+d2 = csv.DictReader(open("courses.csv"))
+
+
+
+for temp1 in d1:
+  print temp1
+  d2 = csv.DictReader(open("courses.csv"))
+  for temp2 in d2:
+    if temp1['id'] == temp2['id']:
+      print temp2
+
+      ##adding to mongo:
+
+  print "-----------"
