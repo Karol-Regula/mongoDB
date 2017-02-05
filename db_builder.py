ec5ed38c55a6d5f8d4754ef842e270da0ef7cbe9
Karol-Regula <kregula@stuy.edu>
2017-02-04 20:41:37 -0500

    Added course dictionaries to student dictionary, added code for adding student dictionary to mongod.

--- db_builder.py
+++ db_builder.py
@@ -8,15 +8,19 @@ c = db.students
 d1 = csv.DictReader(open("peeps.csv"))
 d2 = csv.DictReader(open("courses.csv"))
 
+#so I nested the course dictionaries in the student dictionary
+
 
 
 for temp1 in d1:
-  print temp1
+  print "Student: " + str(temp1)
   d2 = csv.DictReader(open("courses.csv"))
+  i = 0
   for temp2 in d2:
     if temp1['id'] == temp2['id']:
-      print temp2
-
-      ##adding to mongo:
-
+      i += 1
+      temp1['course' + str(i)] = temp2
+      print "Course: " + str(temp2)
+  print "Final??: " + str(temp1)
+  #c.insert_one(temp1) # breaks because it can't connect to the db
   print "-----------"
