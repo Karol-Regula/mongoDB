3e79f4210f03f84a55c93a0004d5f5808a041be1
Bayless <bsmithsalzberg@stuy.edu>
2017-02-05 00:24:40 -0500

    merged both versions

--- db_builder.py
+++ db_builder.py
@@ -1,28 +1,65 @@
 from pymongo import MongoClient
 import csv
 
-server = MongoClient('149.89.150.100')
+
+server = MongoClient('127.0.0.1')
+server.drop_database("KiloByte")
 db = server.KiloByte
 c = db.students
 
 d1 = csv.DictReader(open("peeps.csv"))
-d2 = csv.DictReader(open("courses.csv"))
+d2 = open("courses.csv")
+
+
 
 #so I nested the course dictionaries in the student dictionary
 
+def v1():
+  for temp1 in d1:
+    print "Student: " + str(temp1)
+    d2.seek(0)
+    dicti = csv.DictReader(d2)
+    i = 0
+    for temp2 in dicti:
+      if temp1['id'] == temp2['id']:
+        i += 1
+        temp1['course' + str(i)] = temp2
+    print "Final??: " + str(temp1)
+    c.insert_one(temp1)
+    print "-----------"
 
 
-for temp1 in d1:
-  print "Student: " + str(temp1)
-  d2 = csv.DictReader(open("courses.csv"))
-  i = 0
-  for temp2 in d2:
-    if temp1['id'] == temp2['id']:
-      i += 1
-      temp1['course' + str(i)] = temp2
-      print "Course: " + str(temp2)
-  print "Final??: " + str(temp1)
-  db.c.insert_one(temp1) # breaks because it can't connect to the db
-  print "-----------"
+def checkerV2(): #creds to Evukelj for the boss checker
+  print "Checking that everything was sent accurately! Printing contents\n"
+  for doc in c.find(): #.find() returns all contents
+    print "Student: " + doc['name']
+    print "id: " + str(doc['id'])
+    print "age: " + str(doc['age'])
+    print "course grades:"
+    for course in doc['courses']:
+      print course + " grade: " + str(doc['courses'][course])
+      
+def v2():
+  for peep in d1:
+    info = {} #one dictionary per peep
+    info['name'] = peep['name']
+    info['age'] = peep['age']
+    info['id'] = peep['id']
+    info['courses'] = {}
+    d2.seek(0) #look at courses from beginning
+    courses = csv.DictReader(d2)
+    for student in courses:
+      if peep['id'] == student['id']:
+        info['courses'][student['code']]=student['mark']#filling courses section of peep's dict with the course and grade
+    c.insert_one(info)
+  checkerV2()
 
-print str(db.c.count())
+#v1()
+v2()
+
+
+
+
+
+  
+
