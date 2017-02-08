c83720930fc7f06f2c38c4a9c4052304dd619e7b
Karol-Regula <kregula@stuy.edu>
2017-02-07 21:01:03 -0500

    Fixed small error in homework, seems that pymongo needs to use different dictionaries for each insert or it gets confused with _id duplicates. Cleaned up insert code. hw done!

--- teachers.py
+++ teachers.py
@@ -2,28 +2,30 @@ from pymongo import MongoClient
 import csv, db_builder
 
 
-server = MongoClient('159.89.150.100')
-#server.drop_database("KiloByte")
+server = MongoClient('127.0.0.1')
+server.drop_database("KiloByte")
 db = server.KiloByte
 c = db.students
-
-db_builder.v2()
 
 d3 = csv.DictReader(open("teachers.csv"))
 
 teacherCol = db.teachers
 
 def insert():
-    info = {} #one dictionary per peep
-    for stuff in d3:
-        info['name'] = stuff['teacher']
-        info['period'] = stuff['period']
-        info['code'] = stuff['code']
-        info['studentID'] = []
-        for stu in server.db.students.find({'courses.code':info['code']}):
-            info['studentID'].append(stu['id'])
-    
-    teacherCol.insert_one(info)
+  info = {} #one dictionary per teacher
+  for stuff in d3:
+    info['name'] = stuff['teacher']
+    info['period'] = stuff['period']
+    info['code'] = stuff['code']
+    info['studentID'] = []
+    for doc in c.find():
+      for course in doc['courses']:
+        if (course == info['code']):
+          info['studentID'].append(int(doc['id']))
+    #for stu in server.db.students.find({'courses.code':info['code']}):
+      #info['studentID'].append(stu['id'])
+    print str(info)
+    teacherCol.insert_one(info.copy()) #found this fix, seems that pymongo gets confused when it tries to use the same dictionary over again
 
 def checkerV3(): #creds to Evukelj for the boss checker
   print "Checking that everything was sent accurately! Printing contents\n"
@@ -32,13 +34,10 @@ def checkerV3(): #creds to Evukelj for the boss checker
     print "period: " + str(doc['period'])
     print "code: " + str(doc['code'])
     print "student in class:"
-    for student in doc['studentID']:
-      print course + " student ID: " + str(doc['studentID'][student])
+    print "- student ID: " + str(doc['studentID'])
+    print " "
 
+db_builder.v2()
+#db_builder.checkerV2()
 insert()
 checkerV3()
-
-
-
-
-
